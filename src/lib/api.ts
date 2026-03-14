c4f46ffa8b45753c40f334fc51fef255ab86505b
KavuThao <kavuthao2007@KavuThaos-MacBook-Pro.local>
2026-03-14 22:38:28 +0700

    remove / from API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://bus-api.nkaujntseeg.com/';
+const API_URL = 'https://bus-api.nkaujntseeg.com';
 
 const api = axios.create({
     baseURL: API_URL,
